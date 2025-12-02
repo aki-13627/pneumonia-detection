1af247135dc8a999cc974ac9306af6826921cbcf
aki-13627 <aki.kaku0627@gmail.com>
2025-12-02 15:36:59 +0900

    chage: range of lr_fine

--- utils/hpo.py
+++ utils/hpo.py
@@ -65,7 +65,7 @@ def build_hpo_model(dropout_rate):
 def objective(trial, train_df, val_df):
     
     lr_head = trial.suggest_float("lr_head", 5e-4, 1e-2, log=True)
-    lr_fine = trial.suggest_float("lr_fine", 1e-6, 5e-5, log=True)
+    lr_fine = trial.suggest_float("lr_fine", 1e-6, 1e-4, log=True)
     dropout_rate = trial.suggest_float("dropout_rate", 0.3, 0.6)
     
     frozen_layers_count = trial.suggest_int("frozen_layers_count", 50, 120) 
